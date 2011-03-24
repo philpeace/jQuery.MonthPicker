07eeae635e83d195c6775deb537037b7df44cd35
Phil Peace <phil.totheworld@gmail.com>
2011-03-24 09:57:05 +0000

    updated

--- jquery.monthpicker.js
+++ jquery.monthpicker.js
@@ -1,23 +1,40 @@
-﻿(function ($) {
+﻿/*
+* 	Month Picker Plugin - jQuery plugin
+* 	Simply generates 2 drop down lists as a mask for a text box with a date in it
+*	written by Phil Peace
+*	http://codepeace.blogspot.com/
+*
+*	Copyright (c) 2011 Phil Peace (http://codepeace.blogspot.com)
+*	Dual licensed under the MIT (MIT-LICENSE.txt)
+*	and GPL (GPL-LICENSE.txt) licenses.
+*
+*	Built for jQuery library - Tested on v1.4.x and v1.5.x
+*	http://jquery.com
+*
+*/
+
+(function ($) {
     $.fn.monthPicker = function () {
         return this.each(function () {
             var settings = {
                 'numberOfYears': 10,
-                'dayOfMonth': 1
+                'startYearOffset': -2,
+                'dayOfMonth': 1,
+                'changed': $.noop
             };
 
             var input = $(this);
             input.hide();
-
-            var startDate = new Date();
-            startDate = (input.val().length == 10) ? new Date(parseInt(input.val().substr(6)), parseInt(input.val().substr(3, 2)) - 1, parseInt(input.val().substr(0, 2))) : new Date();
+            var id = input.attr('id');
+            var startDate = new Date(); // Assumes UK date format: dd/MM/yyyy
+            startDate = (input.val().length == 10) ? new Date(parseInt(input.val().substr(6), 10), parseInt(input.val().substr(3, 2), 10) - 1, parseInt(input.val().substr(0, 2), 10)) : new Date();
 
             var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
             var thisYear = (new Date()).getFullYear();
             var startYear = startDate.getFullYear();
             var startMonth = startDate.getMonth();
 
-            var monthSelectHtml = '<select id="' + this.id + '_months' + '">';
+            var monthSelectHtml = '<select id="' + id + '_months" name="' + id + '_months">';
             for (var i = 0; i < 12; i++) {
                 monthSelectHtml += '<option value="' + i + '"';
                 if (i == startMonth) {
@@ -27,8 +44,10 @@
             }
             monthSelectHtml += '</select>';
 
-            var yearSelectHtml = '<select id="' + this.id + '_years' + '">';
-            for (var i = 0; i < settings.numberOfYears; i++) {
+            var yearSelectHtml = '<select id="' + id + '_years' + '">';
+            var numYears = settings.numberOfYears + (settings.startYearOffset * -1);
+            var startYearIndex = settings.startYearOffset;
+            for (var i = startYearIndex; i < numYears; i++) {
                 var newYear = thisYear + i;
                 yearSelectHtml += '<option value="' + newYear + '"'
                 if (newYear == startYear) {
@@ -38,14 +57,29 @@
             }
             yearSelectHtml += '</select>';
 
-            $('<div id="mp-monthpicker-container"><div class="md-monthpicker-selectors">' + monthSelectHtml + yearSelectHtml + '</div></div>').insertAfter(input);
+            $('<div id="mp-monthpicker-container"><div class="mp-monthpicker-selectors">' + monthSelectHtml + yearSelectHtml + '</div></div>').insertAfter(input);
 
-            var monthSelect = $('#' + this.id + '_months');
-            var yearSelect = $('#' + this.id + '_years');
+            var monthSelect = $('#' + id + '_months');
+            var yearSelect = $('#' + id + '_years');
 
-            $(input.parent()).find('select').change(function () {
-                var newDate = settings.dayOfMonth.padLeft(2, '0') + '/' + (parseInt(monthSelect.val()) + 1).padLeft(2, '0') + '/' + yearSelect.val();
-                input.val(newDate);
+            var newDateStr = settings.dayOfMonth.padLeft(2, '0') + '/' + (parseInt(monthSelect.val()) + 1).padLeft(2, '0') + '/' + yearSelect.val();
+            input.attr("value", newDateStr);
+
+            monthSelect.change({ i: '#' + id }, function (event) {
+                var newDateStr = settings.dayOfMonth.padLeft(2, '0') + '/' + (parseInt(monthSelect.val()) + 1).padLeft(2, '0') + '/' + yearSelect.val();
+                $(event.data.i).val(newDateStr);
+
+                var sp = newDateStr.split('/');
+                var newDate = new Date(sp[2], parseInt(sp[1]) - 1, sp[0]);
+                settings.changed(newDate);
+            });
+            yearSelect.change({ i: '#' + id }, function (event) {
+                var newDateStr = settings.dayOfMonth.padLeft(2, '0') + '/' + (parseInt(monthSelect.val()) + 1).padLeft(2, '0') + '/' + yearSelect.val();
+                $(event.data.i).val(newDateStr);
+
+                var sp = newDateStr.split('/');
+                var newDate = new Date(sp[2], parseInt(sp[1]) - 1, sp[0]);
+                settings.changed(newDate);
             });
         });
     };
--- shared.prototype.js
+++ shared.prototype.js
@@ -1,4 +1,6 @@
-﻿String.prototype.padLeft = function (n, pad) {
+﻿// String
+
+String.prototype.padLeft = function (n, pad) {
     var t = '';
     if (n > this.length) {
         for (var i = 0; i < n - this.length; i++) {
@@ -18,6 +20,8 @@ String.prototype.padRight = function (n, pad) {
     return t;
 }
 
+// Number
+
 Number.prototype.padLeft = function (n, pad) {
     return (this + '').padLeft(n, pad);
 }
@@ -26,4 +30,37 @@ Number.prototype.padRight = function (n, pad) {
     return (this + '').padRight(n, pad);
 }
 
+// Date
 
+// For Crockford's json2.js
+Date.prototype.toJSON = function (key) {
+    var j = '/Date(' + this.getTime() + ')/'
+    return j;
+};
+
+// Array
+
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (elt /*, from*/) {
+        var len = this.length >>> 0;
+
+        var from = Number(arguments[1]) || 0;
+        from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+        if (from < 0)
+            from += len;
+
+        for (; from < len; from++) {
+            if (from in this &&
+          this[from] === elt)
+                return from;
+        }
+        return -1;
+    };
+}
+if (!Array.prototype.contains) {
+	Array.prototype.contains = function (key) {
+		return this.indexOf(key) > -1;
+	}
+}
